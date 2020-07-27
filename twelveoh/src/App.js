3c6ab5d726d1219629374942222bbf98d8dcdde6
Harry <harry.mardirossian13@gmail.com>
2020-07-27 04:38:29 -0400

    Cleaned API, added Mount Timer, and harmless Toggle Button

--- twelveoh/src/App.js
+++ twelveoh/src/App.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Toggle from './Toggle';
+import Mcounter from './Counter';
+
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
@@ -11,22 +14,18 @@ function App() {
     });
   }, []);
 
+  
+
+ 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p> The current time is {currentTime}.</p>
+        <div>
+          <p> API-mounted on {currentTime}. </p>
+        </div>
+        <Mcounter></Mcounter>
+        <Toggle></Toggle>
       </header>
     </div>
   );
